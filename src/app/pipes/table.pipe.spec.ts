cc0b97f61df19d5bbf14939c9c972e62581a0445
Venkat <venkat@info-ways.com>
2019-02-24 00:59:32 +0530

    Revert "My sample Project Committed"
    This reverts commit 72c6d5ed9a9cca508c71189b728090a48bedb20b.

--- src/app/pipes/table.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TablePipe } from './table.pipe';
-
-describe('TablePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TablePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
